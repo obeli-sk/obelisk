20f8a69557dbc28784a4b63a01c37d8ef57105c9
Tomas Olvecky <tomas.olvecky@gmail.com>
2023-11-16 17:51:14 +0100

    refactor: Rewrite runtime to async

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -6,17 +6,23 @@ use wasmtime::{
     Config, Engine, Store,
 };
 
-wasmtime::component::bindgen!("example" in "wit/hello-world.wit");
+wasmtime::component::bindgen!({
+    world: "example",
+    path: "../wit/hello-world.wit",
+    async: true,
+});
 
 #[derive(Default)]
 struct Imports {
     slept: bool,
 }
+
+#[async_trait::async_trait]
 impl ExampleImports for Imports {
-    fn sleep(&mut self, millis: u64) -> wasmtime::Result<()> {
+    async fn sleep(&mut self, millis: u64) -> wasmtime::Result<()> {
         if !self.slept {
             println!("Sleeping for {millis}ms");
-            std::thread::sleep(Duration::from_millis(millis));
+            tokio::time::sleep(Duration::from_millis(millis)).await;
             self.slept = true;
         } else {
             println!("Not sleeping");
@@ -25,11 +31,12 @@ impl ExampleImports for Imports {
     }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let wasm = std::env::args().skip(1).next().expect("USAGE: demo WASM");
     // Enable component model (which isn't supported by default)
     let mut config = Config::new();
-    config.wasm_component_model(true);
+    config.async_support(true).wasm_component_model(true);
     // Create a wasmtime execution context
     let engine = Engine::new(&config)?;
     let mut store = Store::new(&engine, Imports::default());
@@ -37,11 +44,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Example::add_to_linker(&mut linker, |state: &mut Imports| state)?;
     // Read and compile the wasm component
     let component = Component::from_file(&engine, wasm)?;
-    // Instantiate a markdown instance
-    let (instance, _) = Example::instantiate(&mut store, &component, &linker)?;
-    let res = instance.call_execute(&mut store)?;
+    // Instantiate the component
+    let (example, _instance) = Example::instantiate_async(&mut store, &component, &linker).await?;
+    let res = example.call_execute(&mut store).await?;
     println!("Got {res}");
-    let res = instance.call_execute(&mut store)?;
+    let res = example.call_execute(&mut store).await?;
     println!("Got {res}");
     Ok(())
 }
