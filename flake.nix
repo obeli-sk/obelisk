3c6f202ab140ff2b06023030cc49f78d02b12059
Tomas Olvecky <tomas.olvecky@gmail.com>
2023-11-16 13:09:26 +0100

    test: Create hello-world component

--- flake.nix
+++ flake.nix
@@ -20,10 +20,17 @@
           };
           rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         in
-        with pkgs;
         {
-          devShells.default = mkShell {
-            buildInputs = [ rustToolchain ];
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustToolchain pkg-config wasm-tools nixpkgs-fmt ];
+            buildInputs = with pkgs; [ openssl ];
+            shellHook = ''
+              project_root="$(git rev-parse --show-toplevel 2>/dev/null)"
+              export CARGO_INSTALL_ROOT="$project_root/.cargo"
+              export PATH="$CARGO_INSTALL_ROOT/bin:$PATH"
+              cargo_packages="cargo-component@0.4.1"
+              cargo install --offline $cargo_packages 2>/dev/null || cargo install $cargo_packages
+            '';
           };
         }
       );
