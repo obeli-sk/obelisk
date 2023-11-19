1340d897cb490233441d58d7c61ba47e525393e6
Tomas Olvecky <tomas.olvecky@gmail.com>
2023-11-19 21:58:20 +0100

    feat: Add wasm-email-provider

---
+++ activities/wasm-email-provider/src/lib.rs
@@ -0,0 +1,22 @@
+mod http;
+
+cargo_component_bindings::generate!();
+
+use crate::bindings::exports::component::wasm_email_provider::email_sender::Email;
+use crate::bindings::exports::component::wasm_email_provider::email_sender::Guest;
+use crate::bindings::wasi::http::types::Method;
+use crate::bindings::wasi::http::types::Scheme;
+
+struct Component;
+
+impl Guest for Component {
+    fn send(
+        _idempotency_id: ::cargo_component_bindings::rt::string::String,
+        _email: Email,
+    ) -> Result<String, String> {
+        let addr = "api.ipify.org";
+        let res = crate::http::request(Method::Get, Scheme::Http, &addr, "", None, None).unwrap();
+        let body = String::from_utf8_lossy(&res.body);
+        Ok(format!("hello {body} from wasm-email-provider"))
+    }
+}
