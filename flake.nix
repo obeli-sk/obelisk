0843139cd1fe4b76dc63165626d5b0ea8bc765f9
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-01-13 16:12:28 +0100

    chore: Bump cargo-component to the next pre-relaese

--- flake.nix
+++ flake.nix
@@ -14,7 +14,22 @@
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          overlays = [ (import rust-overlay) ];
+          overlays = [ (import rust-overlay) (final: prev: {
+              cargo-component = prev.cargo-component.overrideAttrs (old: rec {
+                version = "0.6.0";
+                src = prev.fetchFromGitHub {
+                      owner = "tomasol";
+                      repo = "cargo-component";
+                      rev = "898209357f582b89c52633d47ea7b16b9ddbc203";
+                      sha256 = "sha256-GtLQXNzkFcFt9ciVxvDAxaGJSZNSahvKZpfBnkvanu4=";
+                };
+                cargoDeps = old.cargoDeps.overrideAttrs (pkgs.lib.const {
+                  inherit src;
+                  outputHash = "sha256-sqLSNhmpYpMZhX4QIr6wRB5wJCXBglme1jvyyPtzb5U=";
+                });
+              });
+            })
+          ];
           pkgs = import nixpkgs {
             inherit system overlays;
           };
