7aad547a50116664665580efc2250169b95ac75d
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-02-09 08:40:59 +0100

    bench: Add hyper client for comparison

--- crates/testing/benches/benches/http.rs
+++ crates/testing/benches/benches/http.rs
@@ -77,9 +77,9 @@ fn setup_runtime() -> Runtime {
 
 static COUNTER: std::sync::atomic::AtomicU16 = std::sync::atomic::AtomicU16::new(0);
 
-fn benchmark_http(criterion: &mut Criterion) {
-    const ELEMENTS: u64 = 10;
+const ELEMENTS: u64 = 10;
 
+fn benchmark_activity_client(criterion: &mut Criterion) {
     let _guard = set_up();
     let database = Database::new(100, 100);
     let runtime = setup_runtime();
@@ -94,8 +94,7 @@ fn benchmark_http(criterion: &mut Criterion) {
         server.address().port()
     });
     let fqn = FunctionFqnStr::new("testing:http-workflow/workflow", "execute");
-
-    let mut group = criterion.benchmark_group("http");
+    let mut group = criterion.benchmark_group("benchmark_activity_client");
     group.throughput(Throughput::Elements(ELEMENTS));
     group.bench_function("http", |b| {
         b.to_async::<&tokio::runtime::Runtime>(&RT).iter(|| {
@@ -126,9 +125,76 @@ fn benchmark_http(criterion: &mut Criterion) {
     group.finish();
 }
 
-criterion_group! {
-  name = http_benches;
-  config = Criterion::default();
-  targets = benchmark_http
+fn benchmark_hyper_client(criterion: &mut Criterion) {
+    use http_body_util::Empty;
+    use hyper::body::Bytes;
+    use hyper::Request;
+    use hyper_util::rt::TokioIo;
+    use tokio::net::TcpStream;
+
+    async fn fetch_url(
+        url: &hyper::Uri,
+    ) -> std::result::Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let host = url.host().expect("uri has no host");
+        let port = url.port_u16().unwrap_or(80);
+        let addr = format!("{}:{}", host, port);
+        let stream = TcpStream::connect(addr).await?;
+        let io = TokioIo::new(stream);
+        let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+        tokio::task::spawn(async move {
+            if let Err(err) = conn.await {
+                println!("Connection failed: {:?}", err);
+            }
+        });
+        let authority = url.authority().unwrap().clone();
+        let path = url.path();
+        let req = Request::builder()
+            .uri(path)
+            .header(hyper::header::HOST, authority.as_str())
+            .body(Empty::<Bytes>::new())?;
+        sender.send_request(req).await?;
+        Ok(())
+    }
+    let _guard = set_up();
+    let port = RT.block_on(async {
+        let server = MockServer::start().await;
+        Mock::given(method("GET"))
+            .and(path("/"))
+            .respond_with(ResponseTemplate::new(200).set_body_string(""))
+            .mount(&server)
+            .await;
+        server.address().port()
+    });
+    let url = format!("http://localhost:{port}/").parse().unwrap();
+    let mut group = criterion.benchmark_group("benchmark_hyper_client");
+    group.throughput(Throughput::Elements(ELEMENTS));
+    group.bench_function("http", |b| {
+        b.to_async::<&tokio::runtime::Runtime>(&RT).iter(|| async {
+            let mut futures = Vec::new();
+            for _ in 0..ELEMENTS {
+                futures.push(fetch_url(&url));
+            }
+            futures_util::future::join_all(futures)
+                .await
+                .into_iter()
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap();
+        });
+    });
+    group.finish();
 }
-criterion_main!(http_benches);
+
+criterion_group! {
+  name = benchmark_activity_client_group;
+  config = Criterion::default();
+  targets = benchmark_activity_client
+}
+criterion_group! {
+  name = benchmark_hyper_client_group;
+  config = Criterion::default();
+  targets = benchmark_hyper_client
+}
+criterion_main!(
+    benchmark_activity_client_group,
+    benchmark_hyper_client_group
+);
