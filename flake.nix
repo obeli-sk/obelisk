2390e4d200253e34b1c220a12820d7d284be9073
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-02-12 20:22:32 +0100

    chore: Bump rustc 1.75 → 1.76, cargo-component, flake.lock

--- flake.nix
+++ flake.nix
@@ -14,22 +14,7 @@
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          overlays = [ (import rust-overlay) (final: prev: {
-              cargo-component = prev.cargo-component.overrideAttrs (old: rec {
-                version = "0.6.0";
-                src = prev.fetchFromGitHub {
-                      owner = "tomasol";
-                      repo = "cargo-component";
-                      rev = "c9ac8531f8ecfb1df07a0ce48fa6ec241f9eb630";
-                      sha256 = "sha256-B50wIMKuyYjxPLRUH1pekgcpAs7LI+MR/heDihehEnU=";
-                };
-                cargoDeps = old.cargoDeps.overrideAttrs (pkgs.lib.const {
-                  inherit src;
-                  outputHash = "sha256-sNdjatP22TSxWz8mJN91sAjx6nVOAf6hZYMPEeRRHe0=";
-                });
-              });
-            })
-          ];
+          overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs {
             inherit system overlays;
           };
