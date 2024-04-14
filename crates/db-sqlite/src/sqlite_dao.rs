83f7a3ad5e56dc3361790e6388e502778ff1f149
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-04-14 13:28:07 +0200

    fix: Work around `DatabaseBusy` on pool creation if the db is new

--- crates/db-sqlite/src/sqlite_dao.rs
+++ crates/db-sqlite/src/sqlite_dao.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::all, dead_code)]
 
-use async_sqlite::{JournalMode, Pool, PoolBuilder};
+use async_sqlite::{ClientBuilder, JournalMode, Pool, PoolBuilder};
 use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 use concepts::{
@@ -13,9 +13,12 @@ use concepts::{
     ExecutionId, FunctionFqn,
 };
 use std::{ops::Deref, path::Path};
+use tracing::{debug, trace};
 
 const PRAGMA: &str = r"
 PRAGMA synchronous = NORMAL;
+PRAGMA foreign_keys = true;
+PRAGMA busy_timeout = 1000;
 ";
 
 const EXECUTION_LOG_INIT: &str = r"
@@ -36,8 +39,11 @@ pub struct SqlitePool {
 impl SqlitePool {
     async fn init(pool: &Pool) -> Result<(), async_sqlite::Error> {
         pool.conn(|conn| {
+            trace!("Executing `PRAGMA`");
             conn.execute(PRAGMA, [])?;
+            trace!("Executing `EXECUTION_LOG_INIT`");
             conn.execute(EXECUTION_LOG_INIT, [])?;
+            debug!("Done setting up sqlite");
             Ok(())
         })
         .await?;
@@ -45,6 +51,14 @@ impl SqlitePool {
     }
 
     pub async fn new<P: AsRef<Path>>(path: P) -> Result<Self, async_sqlite::Error> {
+        // Work around a race condition when creating a new database file returns "Database Busy" on one of the threads.
+        // https://github.com/ryanfowler/async-sqlite/issues/10
+        let client = ClientBuilder::new()
+            .path(&path)
+            .journal_mode(JournalMode::Wal)
+            .open()
+            .await?;
+        client.close().await?;
         let pool = PoolBuilder::new()
             .path(path)
             .journal_mode(JournalMode::Wal)
@@ -52,6 +66,10 @@ impl SqlitePool {
             .await?;
         Self::init(&pool).await?;
         Ok(Self { pool })
+    }
+
+    pub async fn close(&self) -> Result<(), async_sqlite::Error> {
+        self.pool.close().await
     }
 }
 
@@ -130,12 +148,12 @@ impl Deref for SqlitePool {
 
 #[cfg(all(test, not(madsim)))] // attempt to spawn a system thread in simulation
 mod tests {
-    use tempfile::NamedTempFile;
-
     use super::SqlitePool;
+    use tempfile::NamedTempFile;
 
     #[tokio::test]
     async fn check_sqlite_version() {
+        test_utils::set_up();
         let file;
         let pool = {
             if let Ok(path) = std::env::var("SQLITE_FILE") {
@@ -146,10 +164,11 @@ mod tests {
                 SqlitePool::new(path).await.unwrap()
             }
         };
-        let version: String = pool
+        let version = pool
             .conn(|conn| conn.query_row("SELECT SQLITE_VERSION()", [], |row| row.get(0)))
-            .await
-            .unwrap();
+            .await;
+        let version: String = version.unwrap();
         assert_eq!("3.45.0", version);
+        pool.close().await.unwrap();
     }
 }
