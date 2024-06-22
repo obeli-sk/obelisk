fae57d0a57cf43a04c6b0890157b626aec535248
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-06-22 19:23:13 +0200

    chore: Build static executable with musl for docker image

--- flake.nix
+++ flake.nix
@@ -15,50 +15,65 @@
       (system:
         let
           overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
           rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-          obelisk = pkgs.rustPlatform.buildRustPackage {
-            pname = "obelisk";
-            version = "0.0.1";
-            src = ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              outputHashes = {
-                "getrandom-0.2.11" = "sha256-fBPB5ptPPBQqvsxTJd+LwKXBdChrVm75DQewyQUhM2Q=";
+          makePkgs = config:
+            import nixpkgs
+              {
+                inherit system overlays;
+                crossSystem =
+                  if config == "x86_64-unknown-linux-musl" then {
+                    inherit config;
+                    rustc = { inherit config; };
+                    isStatic = true;
+                  } else null;
               };
+          makeObelisk = pkgs:
+            pkgs.rustPlatform.buildRustPackage {
+              pname = "obelisk";
+              version = "0.0.1";
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                outputHashes = {
+                  "getrandom-0.2.11" = "sha256-fBPB5ptPPBQqvsxTJd+LwKXBdChrVm75DQewyQUhM2Q=";
+                };
+              };
+              nativeBuildInputs = [ pkgs.pkg-config ];
+              PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+              doCheck = false;
             };
-            nativeBuildInputs = [ pkgs.pkg-config ];
-            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-            doCheck = false;
-          };
-          docker = pkgs.dockerTools.buildImage {
-            name = "obelisk";
-            config = { Entrypoint = [ "${obelisk}/bin/obelisk" ]; };
-          };
+          makeDocker = pkgs: obelisk:
+            pkgs.dockerTools.buildImage {
+              name = "obelisk";
+              config = { Entrypoint = [ "${obelisk}/bin/obelisk" ]; };
+            };
+          pkgs = makePkgs null;
+          pkgsMusl = makePkgs "x86_64-unknown-linux-musl";
         in
         {
           devShells.default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              cargo-binstall
-              cargo-component
-              cargo-dist
-              cargo-expand
-              cargo-insta
-              cargo-nextest
-              litecli
-              lldb
-              nixpkgs-fmt
-              pkg-config
-              rustToolchain
-              tokio-console
-              wasm-tools
-            ];
+            nativeBuildInputs = with pkgs;
+              [
+                cargo-binstall
+                cargo-component
+                cargo-dist
+                cargo-expand
+                cargo-insta
+                cargo-nextest
+                litecli
+                lldb
+                nixpkgs-fmt
+                pkg-config
+                rustToolchain
+                tokio-console
+                wasm-tools
+              ];
           };
 
-          packages = {
-            inherit obelisk docker;
+          packages = rec {
+            obelisk = makeObelisk pkgs;
+            obeliskMusl = makeObelisk pkgsMusl;
+            docker = makeDocker pkgsMusl obeliskMusl;
             default = obelisk;
           };
         }
