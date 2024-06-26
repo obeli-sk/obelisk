f8142f1cc8c357828fa9dbc94feebec85979629b
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-06-26 09:20:02 +0200

    chore: Switch http test activity client to `waki`

--- crates/testing/test-programs/http/activity/src/lib.rs
+++ crates/testing/test-programs/http/activity/src/lib.rs
@@ -1,11 +1,6 @@
-#![cfg(feature = "wasm")]
-
-mod http;
-
 mod bindings;
 
-use crate::bindings::wasi::http::types::Method;
-use crate::bindings::wasi::http::types::Scheme;
+use std::time::Duration;
 
 bindings::export!(Component with_types_in bindings);
 
@@ -13,35 +8,27 @@ struct Component;
 
 impl crate::bindings::exports::testing::http::http_get::Guest for Component {
     fn get(authority: String, path_with_query: String) -> Result<String, String> {
-        crate::http::request(
-            Method::Get,
-            Scheme::Http,
-            &authority,
-            &path_with_query,
-            None,
-            None,
-        )
-        .map(|resp| String::from_utf8_lossy(&resp.body).to_string())
-        .map_err(|err| format!("{err:?}"))
+        let resp = waki::Client::new()
+            .get(&format!("http://{authority}{path_with_query}"))
+            .connect_timeout(Duration::from_secs(5))
+            .send()
+            .map_err(|err| format!("{err:?}"))?;
+        let body = resp.body().map_err(|err| format!("{err:?}"))?;
+        Ok(String::from_utf8_lossy(&body).into_owned())
     }
 
     fn get_successful(authority: String, path_with_query: String) -> Result<String, String> {
-        match crate::http::request(
-            Method::Get,
-            Scheme::Http,
-            &authority,
-            &path_with_query,
-            None,
-            None,
-        ) {
-            Ok(resp) => {
-                if resp.status >= 200 && resp.status <= 299 {
-                    Ok(String::from_utf8_lossy(&resp.body).to_string())
-                } else {
-                    Err(format!("wrong status code: {}", resp.status))
-                }
-            }
-            Err(err) => Err(format!("{err:?}")),
+        let url = format!("http://{authority}{path_with_query}");
+        let resp = waki::Client::new()
+            .get(&url)
+            .connect_timeout(Duration::from_secs(5))
+            .send()
+            .map_err(|err| format!("{err:?}"))?;
+        if resp.status_code() >= 200 && resp.status_code() <= 299 {
+            let body = resp.body().map_err(|err| format!("{err:?}"))?;
+            Ok(String::from_utf8_lossy(&body).into_owned())
+        } else {
+            Err(format!("wrong status code: {}", resp.status_code()))
         }
     }
 }
