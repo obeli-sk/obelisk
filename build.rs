82c148e3bf7ebdda1934fef37d29e6920b6c87e1
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-08-08 09:48:20 +0200

    chore: Add experimental flag for old protoc versions

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     shadow_rs::new()?;
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional") // not needed anymore with protoc  25.3
         .compile_well_known_types(true)
         .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
         .extern_path(".google.protobuf.Any", "::prost_wkt_types::Any")
