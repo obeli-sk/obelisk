c892847fabe60b9d41d6c2ace3c4b863e97967a1
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-09-13 09:26:02 +0200

    chore: Update scripts and config

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -8,3 +8,5 @@ cd $(dirname "$0")/..
 cargo clippy --workspace --all-targets --fix --allow-dirty --allow-staged -- -D warnings
 RUSTFLAGS="--cfg madsim" cargo clippy --workspace --target-dir=target/debug/madsim --fix --allow-dirty --allow-staged --all-targets  -- -D warnings
 cargo fmt
+
+git status -s
