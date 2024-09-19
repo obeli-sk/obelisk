0b7043d8aa831ca5f8254de08b08ffcf89ad6fec
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-09-19 19:31:38 +0200

    chore: Bump to latest `example_activity_http_get`

--- crates/testing/test-programs/http/activity/src/lib.rs
+++ crates/testing/test-programs/http/activity/src/lib.rs
@@ -13,6 +13,7 @@ impl crate::bindings::exports::testing::http::http_get::Guest for Component {
             .connect_timeout(Duration::from_secs(1))
             .send()
             .map_err(|err| format!("{err:?}"))?;
+        println!("Got status code {}", resp.status_code());
         let body = resp.body().map_err(|err| format!("{err:?}"))?;
         Ok(String::from_utf8_lossy(&body).into_owned())
     }
@@ -23,6 +24,7 @@ impl crate::bindings::exports::testing::http::http_get::Guest for Component {
             .connect_timeout(Duration::from_secs(1))
             .send()
             .map_err(|err| format!("{err:?}"))?;
+        println!("Got status code {}", resp.status_code());
         if resp.status_code() >= 200 && resp.status_code() <= 299 {
             let body = resp.body().map_err(|err| format!("{err:?}"))?;
             Ok(String::from_utf8_lossy(&body).into_owned())
