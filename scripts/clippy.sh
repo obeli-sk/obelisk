87e42915e2d87d9c709eae1f0bd6d95a8196c800
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-09-22 21:13:21 +0200

    chore: Regenerate bindings in `clippy.sh`

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -5,6 +5,7 @@
 set -exuo pipefail
 cd $(dirname "$0")/..
 
+cargo component check --workspace # regenerate bindings
 cargo clippy --workspace --all-targets --fix --allow-dirty --allow-staged -- -D warnings
 RUSTFLAGS="--cfg madsim" cargo clippy --workspace --target-dir=target/debug/madsim --fix --allow-dirty --allow-staged --all-targets  -- -D warnings
 cargo fmt
