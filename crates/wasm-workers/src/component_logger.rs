fcc7ae5c964cdd237cf35bbe01b783deb76bfff3
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-09-28 07:16:34 +0200

    logging: Log component messages with a separate target

--- crates/wasm-workers/src/component_logger.rs
+++ crates/wasm-workers/src/component_logger.rs
@@ -2,27 +2,27 @@ use tracing::{debug, error, info, trace, warn, Span};
 
 pub(crate) struct ComponentLogger {
     pub(crate) span: Span,
-    // TODO: level configuration
+    // TODO: dynamic level filtering
 }
-
+const TARGET: &str = "app";
 impl ComponentLogger {
     pub(crate) fn trace(&self, message: &str) {
-        self.span.in_scope(|| trace!("{}", message));
+        self.span.in_scope(|| trace!(target: TARGET, "{}", message));
     }
 
     pub(crate) fn debug(&self, message: &str) {
-        self.span.in_scope(|| debug!("{}", message));
+        self.span.in_scope(|| debug!(target: TARGET, "{}", message));
     }
 
     pub(crate) fn info(&self, message: &str) {
-        self.span.in_scope(|| info!("{}", message));
+        self.span.in_scope(|| info!(target: TARGET, "{}", message));
     }
 
     pub(crate) fn warn(&self, message: &str) {
-        self.span.in_scope(|| warn!("{}", message));
+        self.span.in_scope(|| warn!(target: TARGET, "{}", message));
     }
 
     pub(crate) fn error(&self, message: &str) {
-        self.span.in_scope(|| error!("{}", message));
+        self.span.in_scope(|| error!(target: TARGET, "{}", message));
     }
 }
