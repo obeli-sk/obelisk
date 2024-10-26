0528f0f23d0cd9c347184202dcf1651511fbbc5b
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-10-26 15:54:46 +0200

    fix: Make `webui-builder` a build dependency

---
+++ crates/webui-proxy/build.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // Just here to trigger webui-builder's build.rs
+    webui_builder::dummy();
+}
--- crates/webui/builder/src/lib.rs
+++ crates/webui/builder/src/lib.rs
@@ -1,1 +1,2 @@
-
+// Must be called from webui-proxy build.rs in order to run this package's build.rs
+pub fn dummy() {}
