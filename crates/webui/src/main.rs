88aa35dad0cad58f33e56dc99fc6db9958542b8e
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-10-28 19:33:33 +0100

    chore: Add logging to `webui`

--- crates/webui/src/main.rs
+++ crates/webui/src/main.rs
@@ -121,5 +121,19 @@ fn app() -> Html {
 }
 
 fn main() {
+    init_logging();
     yew::Renderer::<App>::new().render();
+}
+
+fn init_logging() {
+    use log::Level;
+    use wasm_logger::Config;
+
+    // use debug level for debug builds, warn level for production builds.
+    #[cfg(debug_assertions)]
+    let level = Level::Debug;
+    #[cfg(not(debug_assertions))]
+    let level = Level::Warn;
+
+    wasm_logger::init(Config::new(level));
 }
