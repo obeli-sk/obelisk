bff467c5ebd55ca9dcb13dbbc4d3d2d037b982ca
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-10-31 11:13:58 +0100

    fix: Set different `target-dir` in the nested cargo to avoid deadlock

--- crates/webui/builder/build.rs
+++ crates/webui/builder/build.rs
@@ -1,5 +1,8 @@
 use cargo_metadata::camino::Utf8Path;
-use std::{path::Path, process::Command};
+use std::{
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 fn main() {
     let pkg_name = std::env::var("CARGO_PKG_NAME").unwrap();
@@ -19,11 +22,22 @@ fn main() {
 }
 
 fn run_trunk_build(current_dir: &Path) {
+    let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
     let mut cmd = Command::new("trunk");
     cmd.current_dir(current_dir)
         .arg("build")
+        .env("CARGO_TARGET_DIR", &out_dir) // Avoid deadlock when outer cargo holds the lock to the target folder.
         .env_remove("CARGO_ENCODED_RUSTFLAGS")
         .env_remove("CLIPPY_ARGS"); // do not pass clippy parameters
+    if std::env::var("RUST_LOG").is_ok() {
+        let trunk_output_file = out_dir.join("trunk-output.log");
+        println!("cargo:warning=Saving trunk output to {trunk_output_file:?}");
+        let trunk_output_file = std::fs::File::create(trunk_output_file).unwrap();
+        cmd.stdout(std::process::Stdio::from(
+            trunk_output_file.try_clone().unwrap(),
+        )) // Redirect stdout
+        .stderr(std::process::Stdio::from(trunk_output_file)); // Redirect stderr
+    }
     let status = cmd.status().unwrap();
     assert!(status.success());
 }
