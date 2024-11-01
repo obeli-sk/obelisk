c773023348c955e46aa7196ddc83be70a33ed3f4
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-11-01 12:45:17 +0100

    refactor: Move `ListComponents` fetch to `<App />`

--- crates/webui/src/app.rs
+++ crates/webui/src/app.rs
@@ -1,6 +1,16 @@
-use crate::pages::{component_list_page::ComponentListPage, not_found::NotFound};
+use crate::{
+    grpc_client,
+    pages::{component_list_page::ComponentListPage, not_found::NotFound},
+};
+use log::debug;
+use std::ops::Deref;
 use yew::prelude::*;
 use yew_router::prelude::*;
+
+#[derive(Clone, PartialEq, Default)]
+pub struct AppState {
+    pub components: Option<Vec<grpc_client::Component>>,
+}
 
 #[derive(Clone, Routable, PartialEq)]
 pub enum Route {
@@ -41,9 +51,41 @@ impl Route {
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let app_state = use_state(|| AppState::default());
+    {
+        // Make gRPC request to `list_components`.
+        let app_state = app_state.clone();
+        use_effect_with((), move |_| {
+            let app_state = app_state.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let base_url = "/api";
+                let mut fn_repo_client =
+                    grpc_client::function_repository_client::FunctionRepositoryClient::new(
+                        tonic_web_wasm_client::Client::new(base_url.to_string()),
+                    );
+                let mut response = fn_repo_client
+                    .list_components(grpc_client::ListComponentsRequest {
+                        extensions: true,
+                        ..Default::default()
+                    })
+                    .await
+                    .unwrap()
+                    .into_inner();
+                debug!("Got gRPC ListComponentsResponse");
+                response.components.sort_by(|a, b| a.name.cmp(&b.name));
+                app_state.set(AppState {
+                    components: Some(response.components),
+                });
+            });
+            || ()
+        });
+    }
+
     html! {
-        <BrowserRouter>
-            <Switch<Route> render={Route::render} />
-        </BrowserRouter>
+         <ContextProvider<AppState> context={app_state.deref().clone()}>
+            <BrowserRouter>
+                <Switch<Route> render={Route::render} />
+            </BrowserRouter>
+        </ContextProvider<AppState>>
     }
 }
--- crates/webui/src/pages/component_list_page.rs
+++ crates/webui/src/pages/component_list_page.rs
@@ -1,39 +1,9 @@
-use crate::{components::component_list::ComponentTree, grpc_client};
-use log::debug;
-use std::ops::Deref;
+use crate::{app::AppState, components::component_list::ComponentTree, grpc_client};
 use yew::prelude::*;
 
 #[function_component(ComponentListPage)]
 pub fn component_list_page() -> Html {
     let extensions_state = use_state(|| false);
-    let components_state = use_state(|| None);
-
-    {
-        // Make gRPC request to `list_components`.
-        let components_state = components_state.clone();
-        use_effect_with((), move |_| {
-            let components_state = components_state.clone();
-            wasm_bindgen_futures::spawn_local(async move {
-                let base_url = "/api";
-                let mut fn_repo_client =
-                    grpc_client::function_repository_client::FunctionRepositoryClient::new(
-                        tonic_web_wasm_client::Client::new(base_url.to_string()),
-                    );
-                let mut response = fn_repo_client
-                    .list_components(grpc_client::ListComponentsRequest {
-                        extensions: true,
-                        ..Default::default()
-                    })
-                    .await
-                    .unwrap()
-                    .into_inner();
-                debug!("Got gRPC ListComponentsResponse");
-                response.components.sort_by(|a, b| a.name.cmp(&b.name));
-                components_state.set(Some(response.components));
-            });
-            || ()
-        });
-    }
     // Save flipped `extensions_state`.
     let on_extensions_change = {
         let extensions_state = extensions_state.clone();
@@ -42,17 +12,20 @@ pub fn component_list_page() -> Html {
             extensions_state.set(!*extensions_state);
         })
     };
+    let app_state = use_context::<AppState>().expect("AppState context not found");
 
     html! {<>
         <h1>{ "Obelisk WebUI" }</h1>
         <div>
-            <h3>{"Components"}</h3>
-            <p>
-                <input type="checkbox" checked={*extensions_state} onclick={&on_extensions_change} />
-                <label onclick={&on_extensions_change}> { "Show function extensions" }</label>
-            </p>
-            if let Some(components) = components_state.deref() {
+            if let Some(components) = app_state.components {
+                <h3>{"Components"}</h3>
+                <p>
+                    <input type="checkbox" checked={*extensions_state} onclick={&on_extensions_change} />
+                    <label onclick={&on_extensions_change}> { "Show function extensions" }</label>
+                </p>
                 <ComponentTree components={components.clone()} show_extensions={ *extensions_state }/>
+            } else {
+                <p>{"Loading..."}</p>
             }
         </div>
     </>}
