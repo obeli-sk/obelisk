6a4e484fb4bbac1c8305b76f715b84e805bef654
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-11-13 21:30:06 +0100

    feat(ui): Split `ExecutionId` into clickable hierarchy

--- crates/webui/src/grpc/execution_id.rs
+++ crates/webui/src/grpc/execution_id.rs
@@ -1,6 +1,35 @@
-use super::grpc_client;
+use super::grpc_client::{self};
 use std::{fmt::Display, str::FromStr};
 use yew::{html, ToHtml};
+
+pub trait ExecutionIdExt {
+    // For the top-level ExecutionId return [(execution_id.to_string(), execution_id)].
+    // For a derived ExecutionId, return [(grandparent_id.to_string(), grandparent_id), (parent_index, parent_id), .. (child_index, child_id)].
+    fn as_hierarchy(&self) -> Vec<(String, grpc_client::ExecutionId)>;
+}
+
+pub const EXECUTION_ID_INFIX: &str = ".";
+
+impl ExecutionIdExt for grpc_client::ExecutionId {
+    fn as_hierarchy(&self) -> Vec<(String, grpc_client::ExecutionId)> {
+        let mut execution_id = String::new();
+        let mut vec = Vec::new();
+        for part in self.id.split(EXECUTION_ID_INFIX) {
+            execution_id = if execution_id.is_empty() {
+                part.to_string()
+            } else {
+                format!("{execution_id}{EXECUTION_ID_INFIX}{part}")
+            };
+            vec.push((
+                part.to_string(),
+                grpc_client::ExecutionId {
+                    id: execution_id.clone(),
+                },
+            ));
+        }
+        vec
+    }
+}
 
 impl Display for grpc_client::ExecutionId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
--- crates/webui/src/pages/execution_detail_page.rs
+++ crates/webui/src/pages/execution_detail_page.rs
@@ -1,6 +1,10 @@
+use crate::app::Route;
 use crate::components::execution_status::ExecutionStatus;
+use crate::grpc::execution_id::{ExecutionIdExt, EXECUTION_ID_INFIX};
 use crate::grpc::grpc_client;
+use log::debug;
 use yew::prelude::*;
+use yew_router::prelude::Link;
 
 #[derive(Properties, PartialEq)]
 pub struct ExecutionDetailPageProps {
@@ -12,10 +16,27 @@ pub fn execution_detail_page(
 ) -> Html {
     // let app_state =
     //     use_context::<AppState>().expect("AppState context is set when starting the App");
+    let execution_parts = execution_id.as_hierarchy();
+    debug!("execution_parts {execution_parts:?}");
+    let execution_parts: Vec<_> = execution_parts
+        .into_iter()
+        .enumerate()
+        .map(|(idx, (part, execution_id))| {
+            html! {<>
+            if idx > 0 {
+                {EXECUTION_ID_INFIX}
+            }
+            <Link<Route> to={Route::ExecutionDetail { execution_id } }>
+                {part}
+            </Link<Route>>
+            </>}
+        })
+        .collect();
 
     html! {
         <>
-        <h3>{execution_id.to_string() }</h3>
+
+        <h3>{ execution_parts }</h3>
         <ExecutionStatus execution_id={execution_id.clone()} status={None} />
     </>}
     // match FunctionFqn::from_str(ffqn).and_then(|ffqn| {
