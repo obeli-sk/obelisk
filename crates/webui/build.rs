568e0035c7a677539e6fe45aa4e660f7d2020d60
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-11-15 15:14:03 +0100

    feat(grpc): Add definitions for execution history

--- crates/webui/build.rs
+++ crates/webui/build.rs
@@ -6,6 +6,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .protoc_arg("--experimental_allow_proto3_optional") // not needed anymore with protoc  25.3
         .compile_well_known_types(true)
         .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
+        .extern_path(".google.protobuf.Duration", "::prost_wkt_types::Duration")
         .extern_path(".google.protobuf.Any", "::prost_wkt_types::Any")
         .build_server(false)
         .build_client(true)
