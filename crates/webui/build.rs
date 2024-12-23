8fc78650fb712196d05eba9cc59a1bfc4d406fa7
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-12-23 14:14:57 +0100

    refactor: Remove `blueprint.css` from git

--- crates/webui/build.rs
+++ crates/webui/build.rs
@@ -1,7 +1,8 @@
-use std::{error::Error, path::PathBuf};
+use std::{error::Error, fs::File, io::Write, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let proto_path = get_workspace_dir().join("proto");
+    let workspace_dir = get_workspace_dir();
+    let proto_path = workspace_dir.join("proto");
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional") // not needed anymore with protoc  25.3
         .compile_well_known_types(true)
@@ -11,6 +12,24 @@ fn main() -> Result<(), Box<dyn Error>> {
         .build_server(false)
         .build_client(true)
         .compile_protos(&["obelisk.proto"], &[proto_path])?;
+
+    let pkg_name = std::env::var("CARGO_PKG_NAME").unwrap();
+    let meta = cargo_metadata::MetadataCommand::new().exec().unwrap();
+    let package = meta
+        .packages
+        .iter()
+        .find(|p| p.name == pkg_name)
+        .unwrap_or_else(|| panic!("package `{pkg_name}` must exist"));
+    let blueprint_css_path = &package
+        .manifest_path
+        .parent()
+        .unwrap()
+        .join("blueprint.css");
+    if !blueprint_css_path.exists() {
+        let mut blueprint_css_file = File::create(blueprint_css_path)?;
+        blueprint_css_file.write_all(yewprint_css::BLUEPRINT_CSS.as_bytes())?;
+        blueprint_css_file.flush()?;
+    }
     Ok(())
 }
 
