874ce6f12ac77f2f6383576eb350d3a66e0e5d48
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-12-27 13:43:19 +0100

    feat(ui): Type check parameters before submission

--- crates/webui/src/components/execution_submit.rs
+++ crates/webui/src/components/execution_submit.rs
@@ -1,9 +1,62 @@
 use crate::{app::Route, grpc::ffqn::FunctionFqn, grpc::grpc_client};
-use log::{debug, error, trace};
+use log::{debug, error, trace, warn};
 use std::ops::Deref;
+use val_json::wast_val::WastValWithType;
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yew_router::hooks::use_navigator;
+
+#[derive(Debug, Clone, PartialEq)]
+struct FormData {
+    param_refs: Vec<NodeRef>,
+    param_errs: Vec<Option<String>>,
+}
+
+impl FormData {
+    fn validate(&mut self, function_detail: &grpc_client::FunctionDetail) -> Result<(), String> {
+        let mut is_err = false;
+        for (idx, param_ref) in self.param_refs.iter().enumerate() {
+            let param_value = param_ref.cast::<HtmlInputElement>().unwrap().value();
+            debug!("oninput[{idx}] value {param_value}");
+            let param_value: serde_json::Value = match serde_json::from_str(&param_value) {
+                Ok(v) => v,
+                Err(err) => {
+                    warn!("oninput[{idx}] - cannot serialize value to JSON - {err:?}");
+                    self.param_errs[idx] = Some(format!("Cannot serialize value to JSON: {err}"));
+                    is_err = true;
+                    continue;
+                }
+            };
+            let type_wrapper = function_detail
+                .params
+                .get(idx)
+                .as_ref()
+                .expect("FunctionDetail.params cardinality must match FormData.param_refs")
+                .r#type
+                .as_ref()
+                .expect("`FunctionParameter.type` is sent")
+                .type_wrapper
+                .as_str();
+            let type_and_value_json =
+                format!("{{\"type\": {type_wrapper}, \"value\": {param_value}}}");
+            match serde_json::from_str::<WastValWithType>(&type_and_value_json) {
+                Ok(_) => {
+                    self.param_errs[idx] = None;
+                }
+                Err(err) => {
+                    warn!("oninput[{idx}] - typecheck error {err:?}");
+                    self.param_errs[idx] = Some(format!("Typecheck error: {err}"));
+                    is_err = true;
+                }
+            }
+        }
+        if is_err {
+            Err("Cannot serialize parameters".to_string())
+        } else {
+            Ok(())
+        }
+    }
+}
 
 #[derive(Properties, PartialEq)]
 pub struct ExecutionSubmitFormProps {
@@ -17,25 +70,36 @@ pub fn execution_submit_form(
 ) -> Html {
     let ffqn = FunctionFqn::from_fn_detail(fn_detail).expect("ffqn should be parseable");
 
-    #[derive(Debug, Clone, PartialEq)]
-    struct FormData {
-        param_refs: Vec<NodeRef>,
-        param_errs: Vec<Option<String>>,
-        request_processing: bool,
-    }
-
+    // disable the submit button while a request is inflight
+    let request_processing_state = use_state(|| false);
     // Initialize form state with default values
     let form_data_state = use_state(|| FormData {
         param_refs: std::iter::repeat_with(NodeRef::default)
             .take(fn_detail.params.len())
             .collect(),
         param_errs: std::iter::repeat_n(None, fn_detail.params.len()).collect(),
-        request_processing: false, // disable the submit button while a request is inflight
     });
-
     let submit_err_state = use_state(|| None);
 
+    use_effect_with(form_data_state.deref().clone(), {
+        let submit_err_state = submit_err_state.clone();
+        let fn_detail = fn_detail.clone();
+        let form_data_state = form_data_state.clone();
+        move |form_data| {
+            debug!("Validating the form after first render");
+            let mut form_data = form_data.clone();
+            if let Err(err) = form_data.validate(&fn_detail) {
+                log::warn!("Validation failed - {err}");
+                submit_err_state.set(Some(err));
+            } else {
+                submit_err_state.set(None);
+            }
+            form_data_state.set(form_data);
+        }
+    });
+
     let on_submit = {
+        let request_processing_state = request_processing_state.clone();
         let form_data_state = form_data_state.clone();
         let submit_err_state = submit_err_state.clone();
         let ffqn = ffqn.clone();
@@ -55,25 +119,22 @@ pub fn execution_submit_form(
                 Ok(params) => params,
                 Err(serde_err) => {
                     error!("Cannot serialize parameters - {serde_err:?}");
-                    submit_err_state.set(Some(format!("Cannot serialize parameters")));
+                    submit_err_state.set(Some("Cannot serialize parameters".to_string()));
                     return;
                 }
             };
             debug!("Params: {params:?}");
             {
                 submit_err_state.set(None);
-                // disable the submit button
-                let mut form_data = form_data_state.deref().clone();
-                form_data.request_processing = true;
-                form_data_state.set(form_data);
+                request_processing_state.set(true); // disable the submit button
             }
 
             wasm_bindgen_futures::spawn_local({
                 let params = params.clone();
                 let ffqn = ffqn.clone();
-                let form_data_state = form_data_state.clone();
                 let submit_err_state = submit_err_state.clone();
                 let navigator = navigator.clone();
+                let request_processing_state = request_processing_state.clone();
                 async move {
                     let base_url = "/api";
                     let mut client =
@@ -89,6 +150,7 @@ pub fn execution_submit_form(
                             function_name: Some(grpc_client::FunctionName::from(ffqn)),
                         })
                         .await;
+                    request_processing_state.set(false); // reenable the submit button
                     trace!("Got gRPC {response:?}");
                     match response {
                         Ok(response) => {
@@ -103,10 +165,6 @@ pub fn execution_submit_form(
                             error!("Got error {err:?}");
                             submit_err_state
                                 .set(Some(format!("Cannot submit the execution - {err}")));
-                            // reenable the submit button
-                            let mut form_data = form_data_state.deref().clone();
-                            form_data.request_processing = false;
-                            form_data_state.set(form_data);
                         }
                     }
                 }
@@ -119,38 +177,32 @@ pub fn execution_submit_form(
         .iter()
         .enumerate()
         .map(|(idx, param)| {
-            let r#type = param
-                .r#type
+            let ty = param.r#type
                 .as_ref()
-                .and_then(|wit_type| wit_type.wit_type.as_deref())
-                .unwrap_or("<unknown_type>");
+                .expect("`FunctionParameter.type` is sent")
+                ;
+            let wit_type = ty.wit_type.as_ref().expect("`WitType.wit_type` is sent");
             let id = format!("param_{ffqn}_{idx}");
 
             let on_param_change = {
-                    let form_data_state = form_data_state.clone();
-                    Callback::from(move |_| {
-                        let mut form_data = form_data_state.deref().clone();
-                        let param_ref = &form_data.param_refs[idx];
-                        let param_value = param_ref.cast::<HtmlInputElement>().unwrap().value();
-                        // TODO: param validation, including serialization to JSON
-                        let param_value: serde_json::Value = match serde_json::from_str(&param_value) {
-                            Ok(v) => {
-                                form_data.param_errs[idx] = None;
-                                form_data_state.set(form_data);
-                                v
-                            },
-                            Err(err) => {
-                                form_data.param_errs[idx] = Some(format!("Cannot serialize value to JSON: {err}"));
-                                form_data_state.set(form_data);
-                                return;
-                            }
-                        };
-                    })
-                };
+                let form_data_state = form_data_state.clone();
+                let submit_err_state = submit_err_state.clone();
+                let fn_detail = fn_detail.clone();
+                move || {
+                    let mut form_data = form_data_state.deref().clone();
+                    if let Err(err) = form_data.validate(&fn_detail) {
+                        log::warn!("Validation failed - {err}");
+                        submit_err_state.set(Some(err));
+                    } else {
+                        submit_err_state.set(None);
+                    }
+                    form_data_state.set(form_data);
+                }
+            };
 
             html! {<p>
-                <label for={id.clone()}>{ format!("{}: {}", param.name, r#type) }</label>
-                <input id={id} type="text" ref={&form_data_state.param_refs[idx]} oninput = {on_param_change} />
+                <label for={id.clone()}>{ format!("{}: {wit_type}", param.name) }</label>
+                <input id={id} type="text" ref={&form_data_state.param_refs[idx]} oninput = {Callback::from(move |_| { on_param_change()})} />
                 if let Some(err) = form_data_state.param_errs.get(idx) {
                     <span style={"color:red"}>{err}</span>
                 }
@@ -161,7 +213,7 @@ pub fn execution_submit_form(
     html! {<>
         <form onsubmit = {on_submit }>
             {for params_html}
-            <button type="submit" disabled={form_data_state.request_processing}>
+            <button type="submit" disabled={*request_processing_state}>
                 {"Submit"}
             </button>
         </form>
