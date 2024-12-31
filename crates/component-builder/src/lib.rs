175b29586971d53a0c9f6bcfc43801f941572b87
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-12-31 09:34:43 +0100

    fix(component-builder): Fix target dir selection, place `gen.rs` to `OUT_DIR`
    Extract `generate_code`.
    Fix off-by-one bug when traversing ancestors of `OUT_DIR` to find the
    target dir.
    Place the generated `gen.rs` in `OUT_DIR` on both test and non-test
    builds.

--- crates/component-builder/src/lib.rs
+++ crates/component-builder/src/lib.rs
@@ -98,7 +98,7 @@ fn get_target_dir() -> PathBuf {
     // Navigate up to the `target` directory
     out_path
         .ancestors()
-        .nth(3) // Move up 3 levels from OUT_DIR to get to 'target'
+        .nth(4) // `out` = 0th, `<crate_name>-<hash>` = 1st, `build` = 2nd, `debug` = 3rd, `target` = 4th
         .expect("Unable to determine target directory")
         .to_path_buf()
 }
@@ -109,94 +109,16 @@ fn get_out_dir() -> PathBuf {
     PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
 }
 
-#[expect(clippy::too_many_lines)]
-fn build_internal(target_tripple: &str, component_type: ComponentType, out_dir: &Path) {
+fn build_internal(target_tripple: &str, component_type: ComponentType, dst_target_dir: &Path) {
     let pkg_name = std::env::var("CARGO_PKG_NAME").unwrap();
     let pkg_name = pkg_name.strip_suffix("-builder").unwrap();
-    let wasm_path = run_cargo_component_build(out_dir, pkg_name, target_tripple);
+    let wasm_path = run_cargo_build(dst_target_dir, pkg_name, target_tripple);
     if std::env::var("RUST_LOG").is_ok() {
-        println!("cargo:warning=Built {wasm_path:?}");
+        println!("cargo:warning=Built `{pkg_name}` - {wasm_path:?}");
     }
-    let engine = {
-        let mut wasmtime_config = wasmtime::Config::new();
-        wasmtime_config.wasm_component_model(true);
-        wasmtime_config.async_support(true);
-        Engine::new(&wasmtime_config).unwrap()
-    };
-    let mut generated_code = String::new();
-    generated_code += &format!(
-        "pub const {name_upper}: &str = {wasm_path:?};\n",
-        name_upper = to_snake_case(pkg_name).to_uppercase()
-    );
-    {
-        enum Value {
-            Map(IndexMap<String, Value>),
-            Leaf(Vec<String>),
-        }
-        fn ser_map(map: &IndexMap<String, Value>, output: &mut String) {
-            for (k, v) in map {
-                match v {
-                    Value::Leaf(vec) => {
-                        for line in vec {
-                            *output += line;
-                            *output += "\n";
-                        }
-                    }
-                    Value::Map(map) => {
-                        *output += &format!("#[allow(clippy::all)]\npub mod r#{k} {{\n");
-                        ser_map(map, output);
-                        *output += "}\n";
-                    }
-                }
-            }
-        }
 
-        let component = WasmComponent::new(wasm_path, &engine, Some(component_type.into()))
-            .expect("cannot decode wasm component");
-        generated_code += "pub mod exports {\n";
-        let mut outer_map: IndexMap<String, Value> = IndexMap::new();
-        for export in component.exim.get_exports_hierarchy_ext() {
-            let ifc_fqn_split = export
-                .ifc_fqn
-                .split_terminator([':', '/', '@'])
-                .map(to_snake_case);
-            let mut map = &mut outer_map;
-            for mut split in ifc_fqn_split {
-                if split.starts_with(|c: char| c.is_numeric()) {
-                    split = format!("_{split}");
-                }
-                if let Value::Map(m) = map
-                    .entry(split)
-                    .or_insert_with(|| Value::Map(IndexMap::new()))
-                {
-                    map = m;
-                } else {
-                    unreachable!()
-                }
-            }
-            let vec = export
-                .fns
-                .iter()
-                .filter(| (_, FunctionMetadata { submittable,.. }) | *submittable )
-                .map(|(function_name, FunctionMetadata{parameter_types, return_type, ..})| {
-                    format!(
-                        "/// {fn}: func{parameter_types}{arrow_ret_type};\npub const r#{name_upper}: (&str, &str) = (\"{ifc}\", \"{fn}\");\n",
-                        name_upper = to_snake_case(function_name).to_uppercase(),
-                        ifc = export.ifc_fqn,
-                        fn = function_name,
-                        arrow_ret_type = if let Some(ret_type) = return_type { format!(" -> {ret_type}") } else { String::new() }
-                    )
-                })
-                .collect();
-            let old_val = map.insert(String::new(), Value::Leaf(vec));
-            assert!(old_val.is_none(), "same interface cannot appear twice");
-        }
-
-        ser_map(&outer_map, &mut generated_code);
-
-        generated_code += "}\n";
-    }
-    std::fs::write(out_dir.join("gen.rs"), generated_code).unwrap();
+    let generated_code = generate_code(&wasm_path, pkg_name, component_type);
+    std::fs::write(get_out_dir().join("gen.rs"), generated_code).unwrap();
 
     let meta = cargo_metadata::MetadataCommand::new().exec().unwrap();
     let package = meta
@@ -219,30 +141,112 @@ fn build_internal(target_tripple: &str, component_type: ComponentType, out_dir: 
     }
 }
 
+fn generate_code(wasm_path: &Path, pkg_name: &str, component_type: ComponentType) -> String {
+    enum Value {
+        Map(IndexMap<String, Value>),
+        Leaf(Vec<String>),
+    }
+
+    fn ser_map(map: &IndexMap<String, Value>, output: &mut String) {
+        for (k, v) in map {
+            match v {
+                Value::Leaf(vec) => {
+                    for line in vec {
+                        *output += line;
+                        *output += "\n";
+                    }
+                }
+                Value::Map(map) => {
+                    *output += &format!("#[allow(clippy::all)]\npub mod r#{k} {{\n");
+                    ser_map(map, output);
+                    *output += "}\n";
+                }
+            }
+        }
+    }
+
+    let engine = {
+        let mut wasmtime_config = wasmtime::Config::new();
+        wasmtime_config.wasm_component_model(true);
+        wasmtime_config.async_support(true);
+        Engine::new(&wasmtime_config).unwrap()
+    };
+    let mut generated_code = String::new();
+    generated_code += &format!(
+        "pub const {name_upper}: &str = {wasm_path:?};\n",
+        name_upper = to_snake_case(pkg_name).to_uppercase()
+    );
+
+    let component = WasmComponent::new(wasm_path, &engine, Some(component_type.into()))
+        .expect("cannot decode wasm component");
+    generated_code += "pub mod exports {\n";
+    let mut outer_map: IndexMap<String, Value> = IndexMap::new();
+    for export in component.exim.get_exports_hierarchy_ext() {
+        let ifc_fqn_split = export
+            .ifc_fqn
+            .split_terminator([':', '/', '@'])
+            .map(to_snake_case);
+        let mut map = &mut outer_map;
+        for mut split in ifc_fqn_split {
+            if split.starts_with(|c: char| c.is_numeric()) {
+                split = format!("_{split}");
+            }
+            if let Value::Map(m) = map
+                .entry(split)
+                .or_insert_with(|| Value::Map(IndexMap::new()))
+            {
+                map = m;
+            } else {
+                unreachable!()
+            }
+        }
+        let vec = export
+                .fns
+                .iter()
+                .filter(| (_, FunctionMetadata { submittable,.. }) | *submittable )
+                .map(|(function_name, FunctionMetadata{parameter_types, return_type, ..})| {
+                    format!(
+                        "/// {fn}: func{parameter_types}{arrow_ret_type};\npub const r#{name_upper}: (&str, &str) = (\"{ifc}\", \"{fn}\");\n",
+                        name_upper = to_snake_case(function_name).to_uppercase(),
+                        ifc = export.ifc_fqn,
+                        fn = function_name,
+                        arrow_ret_type = if let Some(ret_type) = return_type { format!(" -> {ret_type}") } else { String::new() }
+                    )
+                })
+                .collect();
+        let old_val = map.insert(String::new(), Value::Leaf(vec));
+        assert!(old_val.is_none(), "same interface cannot appear twice");
+    }
+
+    ser_map(&outer_map, &mut generated_code);
+    generated_code += "}\n";
+    generated_code
+}
+
 fn add_dependency(file: &Utf8Path) {
     println!("cargo:rerun-if-changed={file}");
 }
 
-fn run_cargo_component_build(out_dir: &Path, name: &str, tripple: &str) -> PathBuf {
+fn run_cargo_build(dst_target_dir: &Path, name: &str, tripple: &str) -> PathBuf {
     let mut cmd = Command::new("cargo");
     cmd.arg("build")
         .arg("--release")
         .arg(format!("--target={tripple}"))
         .arg(format!("--package={name}"))
-        .env("CARGO_TARGET_DIR", out_dir)
+        .env("CARGO_TARGET_DIR", dst_target_dir)
         .env("CARGO_PROFILE_RELEASE_DEBUG", "limited") // keep debuginfo for backtraces
         .env_remove("CARGO_ENCODED_RUSTFLAGS")
         .env_remove("CLIPPY_ARGS"); // do not pass clippy parameters
     let status = cmd.status().unwrap();
     assert!(status.success());
     let name_snake_case = to_snake_case(name);
-    let target = out_dir
+    let target = dst_target_dir
         .join(tripple)
         .join("release")
         .join(format!("{name_snake_case}.wasm",));
     assert!(target.exists(), "Target path must exist: {target:?}");
     if is_transformation_to_wasm_component_needed(tripple) {
-        let target_transformed = out_dir
+        let target_transformed = dst_target_dir
             .join(tripple)
             .join("release")
             .join(format!("{name_snake_case}_component.wasm",));
