0e17a0016e84165cb33b52c82af64ba81bd1c2a6
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-01-10 21:11:10 +0100

    fix: Use snake case in `JoinNextBlockingStrategy` serde

--- crates/wasm-workers/src/workflow/workflow_worker.rs
+++ crates/wasm-workers/src/workflow/workflow_worker.rs
@@ -29,6 +29,7 @@ use wasmtime::{Store, UpdateDeadline};
 #[derive(
     Debug, Default, Clone, Copy, PartialEq, Eq, serde::Serialize, serde::Deserialize, Hash,
 )]
+#[serde(rename_all = "snake_case")]
 pub enum JoinNextBlockingStrategy {
     /// Shut down the current runtime. When the [`JoinSetResponse`] is appended, workflow is reexecuted with a new `RunId`.
     Interrupt,
