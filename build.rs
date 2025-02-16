f8046bde86e752af78d2db1eeaabf5b2802ee253
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-02-16 11:03:19 +0100

    feat(cli): Add `--json` parameter to `submit` and `get`

--- build.rs
+++ build.rs
@@ -8,6 +8,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
         .extern_path(".google.protobuf.Duration", "::prost_wkt_types::Duration")
         .extern_path(".google.protobuf.Any", "::prost_wkt_types::Any")
+        .type_attribute(".", "#[derive(serde::Serialize)]") // for CLI only
+        .type_attribute(".", "#[serde(rename_all=\"snake_case\")]") // for CLI only
         .build_server(true)
         .build_client(true)
         .compile_protos(&["proto/obelisk.proto"], &[] as &[&str])?;
