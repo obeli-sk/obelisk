6da80e7790b2b4bf148a0f1e0a07a03c1b69fd25
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-04-02 20:50:59 +0200

    feat(webui): Display links to trace, log, debug

--- crates/webui/src/components/execution_header.rs
+++ crates/webui/src/components/execution_header.rs
@@ -3,6 +3,7 @@ use crate::components::execution_status::ExecutionStatus;
 use crate::grpc::execution_id::ExecutionIdExt;
 use crate::grpc::grpc_client::ExecutionId;
 use yew::prelude::*;
+use yew_router::prelude::Link;
 
 #[derive(Properties, PartialEq)]
 pub struct ExecutionHeaderProps {
@@ -18,8 +19,24 @@ pub fn execution_header(
 ) -> Html {
     html! {
         <div class="execution-header">
-            <h3>{ execution_id.render_execution_parts(false, route_fn.clone()) }</h3>
+            <div class="header-and-links">
+                <h3>{ execution_id.render_execution_parts(false, route_fn.clone()) }</h3>
+
+                <div class="execution-links">
+                    <Link<Route> to={Route::ExecutionTrace { execution_id: execution_id.clone() }}>
+                        {"Trace"}
+                    </Link<Route>>
+                    <Link<Route> to={Route::ExecutionDetail { execution_id: execution_id.clone() }}>
+                        {"Log"}
+                    </Link<Route>>
+                    <Link<Route> to={Route::ExecutionDebugger { execution_id: execution_id.clone() }}>
+                        {"Debug"}
+                    </Link<Route>>
+                </div>
+            </div>
+
             <ExecutionStatus execution_id={execution_id.clone()} status={None} print_finished_status={true} />
+
         </div>
     }
 }
