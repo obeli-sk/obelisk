85f39b1ee1cb581d0e8d8066c79ef0a81f4bb8d4
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-04-04 10:06:52 +0200

    chore: Interpret "dry-run" tag as a dry run

--- scripts/push-webui.sh
+++ scripts/push-webui.sh
@@ -6,14 +6,13 @@ set -exuo pipefail
 cd "$(dirname "$0")/.."
 
 TAG="$1"
-DRY_RUN="${2:-false}"
 rm -rf crates/webui/dist
 
 # Run trunk in webui-builder
 CARGO_PROFILE_RELEASE_DEBUG=limited RUN_TRUNK=true \
     cargo build --package webui-proxy --target=wasm32-wasip2 --profile=release_trunk
 
-if [ "$DRY_RUN" != "true" ]; then
+if [ "$TAG" != "dry-run" ]; then
     echo -n $(obelisk client component push "target/wasm32-wasip2/release_trunk/webui_proxy.wasm" \
         "docker.io/getobelisk/webui:$TAG") > assets/webui-version.txt
 fi
