bfab148cba127fa6c259b4c441b95fe767fd37e7
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-04-04 14:53:04 +0200

    style: Remove commented code

--- scripts/extract-release-info.sh
+++ scripts/extract-release-info.sh
@@ -6,7 +6,6 @@
 set -exuo pipefail
 cd "$(dirname "$0")/.."
 
-# Check for version argument.
 if [ -z "$1" ]; then
   echo "Usage: $0 <VERSION>"
   exit 1
@@ -16,8 +15,6 @@ VERSION="$1"
 
 START_LINE=$(awk -v version="$VERSION" '$0 ~ "^## \\[" version "\\]" {print NR}' CHANGELOG.md)
 
-# END_LINE=$(tail -n +$((START_LINE + 1)) CHANGELOG.md | awk '/^## /{print NR; exit}')
 END_LINE=$(awk -v start="$((START_LINE + 1))" 'NR >= start && /^## / {print NR; exit}' CHANGELOG.md)
 
-# tail -n +$((START_LINE + 2)) CHANGELOG.md | head -n $((END_LINE - 2))
 sed -n "$((START_LINE + 1)),$((END_LINE - 1))p" CHANGELOG.md
