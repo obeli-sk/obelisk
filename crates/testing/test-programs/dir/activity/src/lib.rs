504627e489a77cec46aac25cf0bf3a15c8b3558b
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-05-29 20:42:30 +0200

    test: Extract `ls`, verify file list

--- crates/testing/test-programs/dir/activity/src/lib.rs
+++ crates/testing/test-programs/dir/activity/src/lib.rs
@@ -1,6 +1,5 @@
-use std::path::PathBuf;
-
 use exports::testing::dir::dir::Guest;
+use std::path::PathBuf;
 use wit_bindgen::generate;
 
 generate!({ generate_all });
@@ -9,21 +8,37 @@ export!(Component);
 
 impl Guest for Component {
     fn io() -> Result<(), String> {
-        let mut entries = std::fs::read_dir(".")
-            .expect("read_dir failed")
-            .map(|res| res.map(|e| e.path()))
-            .collect::<Result<Vec<_>, _>>()
-            .expect("iterating over folder failed");
-        entries.sort();
-        println!("{entries:?}");
-
+        let entries = ls();
+        // Let's put some content idempotently
         let tempfile = PathBuf::from("test.txt");
         std::fs::write(tempfile, b"Lorem ipsum").expect("writing to a file failed");
         std::fs::create_dir_all("foo/bar/baz").expect("create_dir_all failed");
+
         if entries.is_empty() {
-            Err("error on first try".to_string())
-        } else {
-            Ok(())
+            // Should be retried and succeed..
+            return Err(
+                "failing on first try, should be automatically fixed on the first retry if `reuse-on-retry` is enabled"
+                    .to_string(),
+            );
         }
+
+        let entries = ls();
+        if entries != vec!["./foo", "./test.txt"] {
+            return Err(format!("unexpected file list: {entries:?}"));
+        }
+
+        Ok(())
     }
+}
+
+fn ls() -> Vec<String> {
+    let mut entries = std::fs::read_dir(".")
+        .expect("read_dir failed")
+        .map(|res| res.map(|e| e.path().to_string_lossy().into_owned()))
+        .collect::<Result<Vec<_>, _>>()
+        .expect("iterating over folder failed");
+    entries.sort();
+    println!("ls:");
+    println!("{entries:?}");
+    entries
 }
