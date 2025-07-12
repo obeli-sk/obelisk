0a44274d395d9f0662773db9d137fcb9f807d37d
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-07-12 14:54:26 +0200

    bench: Extract db, executors out of `bench` function

--- crates/bench/benches/fibo.rs
+++ crates/bench/benches/fibo.rs
@@ -23,7 +23,8 @@ use utils::wasm_tools::WasmComponent;
 use wasm_workers::activity::activity_worker::{ActivityConfig, ActivityWorker};
 use wasm_workers::engines::Engines;
 use wasm_workers::workflow::workflow_worker::{
-    JoinNextBlockingStrategy, WorkflowConfig, WorkflowWorkerCompiled,
+    DEFAULT_NON_BLOCKING_EVENT_BATCHING, JoinNextBlockingStrategy, WorkflowConfig,
+    WorkflowWorkerCompiled,
 };
 use wasmtime::Engine;
 
@@ -241,31 +242,7 @@ fn fiboa(bencher: divan::Bencher, args: u32) {
     .unwrap();
     let codegen_cache = workspace_dir.join("test-codegen-cache");
     let engines = Arc::new(Engines::on_demand(Some(codegen_cache)).unwrap());
-    // compile before benching
-    compile_activity_with_engine(
-        test_programs_fibo_activity_builder::TEST_PROGRAMS_FIBO_ACTIVITY,
-        &engines.activity_engine,
-    );
-    compile_workflow_with_engine(
-        test_programs_fibo_workflow_builder::TEST_PROGRAMS_FIBO_WORKFLOW,
-        &engines.workflow_engine,
-    );
-    bencher.bench(|| {
-        let engines = engines.clone();
-        rt.block_on(async move { fibo_workflow(1, args, engines).await });
-    });
-}
 
-const FIBO_WORKFLOW_FFQN: FunctionFqn = FunctionFqn::new_static_tuple(
-    test_programs_fibo_workflow_builder::exports::testing::fibo_workflow::workflow::FIBOA,
-); // fiboa: func(n: u8, iterations: u32) -> u64;
-
-async fn fibo_workflow(fibo_n: u8, iterations: u32, engines: Arc<Engines>) {
-    let join_next_blocking_strategy = JoinNextBlockingStrategy::Await {
-        non_blocking_event_batching: 10,
-    };
-
-    let (_guard, db_pool) = Database::Sqlite.set_up().await;
     let fn_registry = TestingFnRegistry::new_from_components(vec![
         compile_activity_with_engine(
             test_programs_fibo_activity_builder::TEST_PROGRAMS_FIBO_ACTIVITY,
@@ -276,13 +253,40 @@ async fn fibo_workflow(fibo_n: u8, iterations: u32, engines: Arc<Engines>) {
             &engines.workflow_engine,
         ),
     ]);
+
+    let (_guard, db_pool) = rt.block_on(async move { Database::Sqlite.set_up().await });
+
     let workflow_exec_task = spawn_workflow_fibo(
         db_pool.clone(),
         Now,
-        join_next_blocking_strategy,
+        JoinNextBlockingStrategy::default(),
         fn_registry,
         engines.workflow_engine.clone(),
     );
+
+    let activity_exec_task = spawn_activity_fibo(
+        db_pool.clone(),
+        Now,
+        TokioSleep,
+        engines.activity_engine.clone(),
+    );
+
+    bencher.bench(|| {
+        let db_pool = db_pool.clone();
+        rt.block_on(async move { fibo_workflow(1, args, db_pool).await });
+    });
+
+    rt.block_on(async move { workflow_exec_task.close().await });
+    rt.block_on(async move { activity_exec_task.close().await });
+
+    rt.block_on(async move { db_pool.close().await.unwrap() });
+}
+
+const FIBO_WORKFLOW_FFQN: FunctionFqn = FunctionFqn::new_static_tuple(
+    test_programs_fibo_workflow_builder::exports::testing::fibo_workflow::workflow::FIBOA,
+); // fiboa: func(n: u8, iterations: u32) -> u64;
+
+async fn fibo_workflow(fibo_n: u8, iterations: u32, db_pool: Arc<dyn DbPool>) {
     // Create an execution.
     let execution_id = ExecutionId::generate();
     let created_at = Now.now();
@@ -306,36 +310,10 @@ async fn fibo_workflow(fibo_n: u8, iterations: u32, engines: Arc<Engines>) {
         .await
         .unwrap();
 
-    wait_for_pending_state_fn(
-        db_connection.as_ref(),
-        &execution_id,
-        |exe_history| {
-            matches!(
-                exe_history.pending_state,
-                PendingState::BlockedByJoinSet { .. }
-            )
-            .then_some(())
-        },
-        None,
-    )
-    .await
-    .unwrap();
-
-    let activity_exec_task = spawn_activity_fibo(
-        db_pool.clone(),
-        Now,
-        TokioSleep,
-        engines.activity_engine.clone(),
-    );
-
     let res = db_connection
         .wait_for_finished_result(&execution_id, None)
         .await
         .unwrap()
         .unwrap();
     assert_matches!(res, SupportedFunctionReturnValue::InfallibleOrResultOk(_));
-
-    workflow_exec_task.close().await;
-    activity_exec_task.close().await;
-    db_pool.close().await.unwrap();
 }
