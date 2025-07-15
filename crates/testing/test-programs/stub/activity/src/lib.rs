3985844b1396fae9e3702b4bef122a77e96712e0
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-07-15 10:25:43 +0200

    test: Add mocked `test_programs_stub_activity`

---
+++ crates/testing/test-programs/stub/activity/src/lib.rs
@@ -0,0 +1,12 @@
+use exports::testing::stub_activity::activity::Guest;
+use wit_bindgen::generate;
+
+generate!({ generate_all });
+struct Component;
+export!(Component);
+
+impl Guest for Component {
+    fn foo(_arg: String) -> String {
+        todo!()
+    }
+}
