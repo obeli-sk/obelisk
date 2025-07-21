0ba05c43cf6580339ef831ad0ebf6a8db519c9f4
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-07-21 14:15:30 +0200

    test: Simplify `submit-await`

--- crates/testing/test-programs/stub/workflow/src/lib.rs
+++ crates/testing/test-programs/stub/workflow/src/lib.rs
@@ -1,3 +1,4 @@
+use crate::testing::stub_activity::activity;
 use crate::testing::stub_activity_obelisk_ext::activity as activity_ext;
 use crate::testing::stub_activity_obelisk_stub::activity as activity_stub;
 use exports::testing::stub_workflow::workflow::Guest;
@@ -21,11 +22,6 @@ impl Guest for Component {
     }
 
     fn submit_await(arg: String) -> String {
-        let join_set = workflow_support::new_join_set_generated(ClosingStrategy::Complete);
-        let execution_id = activity_ext::foo_submit(&join_set, &arg);
-        let (actual_execution_id, ret_val) =
-            activity_ext::foo_await_next(&join_set).expect("stubbed activity must resolve");
-        assert_eq!(execution_id.id, actual_execution_id.id);
-        ret_val
+        activity::foo(&arg)
     }
 }
