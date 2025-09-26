49e42f786d25e0ea865b6fa2fb1305363d07ae75
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-09-26 15:43:32 +0200

    ci: Create PR in `push-test-components`

--- scripts/push-test-components.sh
+++ scripts/push-test-components.sh
@@ -9,12 +9,14 @@ TAG="$1"
 TOML_FILE="obelisk.toml"
 PREFIX="docker.io/getobelisk/"
 
+cargo build
+
 push() {
     RELATIVE_PATH=$1
     FILE_NAME_WITHOUT_EXT=$(basename "$RELATIVE_PATH" | sed 's/\.[^.]*$//')
     OCI_LOCATION="${PREFIX}${FILE_NAME_WITHOUT_EXT}:${TAG}"
     echo "Pushing ${RELATIVE_PATH} to ${OCI_LOCATION}..."
-    OUTPUT=$(obelisk client component push "$RELATIVE_PATH" "$OCI_LOCATION")
+    OUTPUT=$(cargo run -- client component push "$RELATIVE_PATH" "$OCI_LOCATION")
 
     # Replace the old location with the actual OCI location
     sed -i -E "/name = \"${FILE_NAME_WITHOUT_EXT}\"/{n;s|location\.oci = \".*\"|location.oci = \"${OUTPUT}\"|}" "$TOML_FILE"
