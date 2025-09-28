79b2bbde5c2b75a56884cfe55a217aa1948a41e4
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-09-28 22:01:21 +0200

    fix(cli): Ignore old version header in `generate extensions`

--- scripts/regenerate-wit-extensions.sh
+++ scripts/regenerate-wit-extensions.sh
@@ -8,26 +8,26 @@ CARGO_WORKSPACE_DIR=$(pwd)
 
 (
     cd crates/testing/test-programs/fibo/activity/wit
-    rm -rf gen
+    if [ "${RECREATE:-}" = "true" ]; then rm -rf gen; fi
     $CARGO_WORKSPACE_DIR/target/debug/obelisk generate extensions activity_wasm . gen
 )
 (
     cd crates/testing/test-programs/fibo/workflow/wit
-    rm -rf gen
+    if [ "${RECREATE:-}" = "true" ]; then rm -rf gen; fi
     $CARGO_WORKSPACE_DIR/target/debug/obelisk generate extensions workflow . gen
 )
 (
     cd crates/testing/test-programs/http/activity/wit
-    rm -rf gen
+    if [ "${RECREATE:-}" = "true" ]; then rm -rf gen; fi
     $CARGO_WORKSPACE_DIR/target/debug/obelisk generate extensions activity_wasm . gen
 )
 (
     cd crates/testing/test-programs/sleep/activity/wit
-    rm -rf gen
+    if [ "${RECREATE:-}" = "true" ]; then rm -rf gen; fi
     $CARGO_WORKSPACE_DIR/target/debug/obelisk generate extensions activity_wasm . gen
 )
 (
     cd crates/testing/test-programs/stub/activity/wit
-    rm -rf gen
+    if [ "${RECREATE:-}" = "true" ]; then rm -rf gen; fi
     $CARGO_WORKSPACE_DIR/target/debug/obelisk generate extensions activity_stub . gen
 )
