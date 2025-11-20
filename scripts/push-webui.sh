556839dd103bf009e152ae60da5131cdc272a635
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-11-20 16:25:09 +0100

    refactor: Use downloaded Obelisk in `push-webui.sh`

--- scripts/push-webui.sh
+++ scripts/push-webui.sh
@@ -15,7 +15,17 @@ CARGO_PROFILE_RELEASE_DEBUG=limited RUN_TRUNK=true \
     cargo build --package webui-proxy --target=wasm32-wasip2 --profile=release_trunk
 
 if [ "$TAG" != "dry-run" ]; then
-    OUTPUT=$(cargo run -- client component push "target/wasm32-wasip2/release_trunk/webui_proxy.wasm" \
+
+    # Download obelisk into a temp directory
+    TMPDIR="$(mktemp -d)"
+    trap 'rm -rf "$TMPDIR"' EXIT
+    (
+        cd "$TMPDIR"
+        curl -L --tlsv1.2 -sSf https://raw.githubusercontent.com/obeli-sk/obelisk/main/download.sh | bash
+    )
+    OBELISK="$TMPDIR/obelisk"
+
+    OUTPUT=$($OBELISK client component push "target/wasm32-wasip2/release_trunk/webui_proxy.wasm" \
         "docker.io/getobelisk/webui:$TAG")
     echo -n $OUTPUT > assets/webui-version.txt
 fi
