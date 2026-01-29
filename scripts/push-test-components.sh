37c122c39f6d733590dfef50e592a5cb2bf21f60
Tomas Olvecky <tomas.olvecky@gmail.com>
2026-01-29 12:01:13 +0100

    build: Update `push-test-components.sh`

--- scripts/push-test-components.sh
+++ scripts/push-test-components.sh
@@ -11,26 +11,27 @@ PREFIX="docker.io/getobelisk/"
 cargo check --workspace # starts build.rs of each -builder
 
 push() {
-    RELATIVE_PATH=$1
+    COMPONENT_TYPE=$1
+    RELATIVE_PATH=$2
     FILE_NAME_WITHOUT_EXT=$(basename "$RELATIVE_PATH" | sed 's/\.[^.]*$//')
     OCI_LOCATION="${PREFIX}${FILE_NAME_WITHOUT_EXT}:${TAG}"
     echo "Pushing ${RELATIVE_PATH} to ${OCI_LOCATION}..."
     OUTPUT=$(cargo run -- component push "$RELATIVE_PATH" "$OCI_LOCATION")
 
     # Replace the old location with the actual OCI location
-    sed -i -E "/name = \"${FILE_NAME_WITHOUT_EXT}\"/{n;s|location\.oci = \".*\"|location.oci = \"${OUTPUT}\"|}" obelisk-testing-sqlite-oci.toml
-    sed -i -E "/name = \"${FILE_NAME_WITHOUT_EXT}\"/{n;s|location\.oci = \".*\"|location.oci = \"${OUTPUT}\"|}" obelisk-testing-postgres-oci.toml
+    cargo run -- component add ${COMPONENT_TYPE} ${OUTPUT} --name ${FILE_NAME_WITHOUT_EXT} --config obelisk-testing-sqlite-oci.toml
+    cargo run -- component add ${COMPONENT_TYPE} ${OUTPUT} --name ${FILE_NAME_WITHOUT_EXT} --config obelisk-testing-postgres-oci.toml
 }
 
 # Make sure all components are fresh
 cargo check --workspace
 
-push "target/release_testprograms/wasm32-wasip2/release_wasm/test_programs_fibo_activity.wasm"
-push "target/release_testprograms/wasm32-unknown-unknown/release_wasm/test_programs_fibo_workflow.wasm"
-push "target/release_testprograms/wasm32-wasip2/release_wasm/test_programs_fibo_webhook.wasm"
-push "target/release_testprograms/wasm32-wasip2/release_wasm/test_programs_http_get_activity.wasm"
-push "target/release_testprograms/wasm32-unknown-unknown/release_wasm/test_programs_http_get_workflow.wasm"
-push "target/release_testprograms/wasm32-wasip2/release_wasm/test_programs_sleep_activity.wasm"
-push "target/release_testprograms/wasm32-unknown-unknown/release_wasm/test_programs_sleep_workflow.wasm"
+push activity_wasm      "target/release_testprograms/wasm32-wasip2/release_wasm/test_programs_fibo_activity.wasm"
+push workflow           "target/release_testprograms/wasm32-unknown-unknown/release_wasm/test_programs_fibo_workflow.wasm"
+push webhook_endpoint   "target/release_testprograms/wasm32-wasip2/release_wasm/test_programs_fibo_webhook.wasm"
+push activity_wasm      "target/release_testprograms/wasm32-wasip2/release_wasm/test_programs_http_get_activity.wasm"
+push workflow           "target/release_testprograms/wasm32-unknown-unknown/release_wasm/test_programs_http_get_workflow.wasm"
+push activity_wasm      "target/release_testprograms/wasm32-wasip2/release_wasm/test_programs_sleep_activity.wasm"
+push workflow           "target/release_testprograms/wasm32-unknown-unknown/release_wasm/test_programs_sleep_workflow.wasm"
 
 echo "All components pushed and TOML file updated successfully."
