ae2f79e901a1cce2a8ac677327cd4798664985b5
Tomas Olvecky <tomas.olvecky@gmail.com>
2026-02-13 12:48:26 +0100

    test: Add `secret-get` to the http activity

--- crates/testing/test-programs/http/activity/src/lib.rs
+++ crates/testing/test-programs/http/activity/src/lib.rs
@@ -46,4 +46,39 @@ impl Guest for Component {
     fn get_resp(url: String) -> Result<http_get::Response, String> {
         block_on(async { get_resp(url).await }).map_err(|err| err.to_string())
     }
+
+    fn secret_get(
+        url: String,
+        env_var: String,
+        header: Option<(String, String)>,
+    ) -> Result<http_get::ResponseStr, String> {
+        let secret_placeholder =
+            std::env::var(&env_var).map_err(|_| "cannot get env var".to_string())?;
+        println!("Resolved env var: {env_var}={secret_placeholder}");
+        let url = url.replace(&env_var, &secret_placeholder);
+        println!("Connecting to {url:?}");
+        let resp = block_on(async {
+            let request = Request::builder().method(Method::GET).uri(url);
+            let request = if let Some((header_key, header_val)) = header {
+                request.header(
+                    header_key.replace(&env_var, &secret_placeholder),
+                    header_val.replace(&env_var, &secret_placeholder),
+                )
+            } else {
+                request
+            };
+            let request = request.body(Body::empty())?;
+
+            let response = Client::new().send(request).await?;
+            let status_code = response.status().as_u16();
+            let mut response = response.into_body();
+            let body = Vec::from(response.contents().await?);
+            Ok::<_, anyhow::Error>(http_get::Response { body, status_code })
+        })
+        .map_err(|err| err.to_string())?;
+        Ok(http_get::ResponseStr {
+            body: String::from_utf8_lossy(&resp.body).to_string(),
+            status_code: resp.status_code,
+        })
+    }
 }
