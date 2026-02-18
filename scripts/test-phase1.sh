902df386a4a033d5fcf359c6b46383851a55655e
Tomas Olvecky <tomas.olvecky@gmail.com>
2026-02-18 22:39:28 +0100

    Revert "ci: Attempt to fix timeouts while codegen by forcing single threaded-ness"
    This reverts commit 79fd463c9c8035dd40632dd79adc288e51040196.

--- scripts/test-phase1.sh
+++ scripts/test-phase1.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")/.."
 
 cargo nextest run --no-output-indent --workspace --profile ci-test-populate-codegen-cache \
   populate_codegen_cache \
-  ${ADDITIONAL_FEATURES:-} "$@"
+  ${ADDITIONAL_FEATURES:-}
