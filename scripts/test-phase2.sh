ff11e1a4fb7b104ea323f2affb70ae422cfae452
Tomas Olvecky <tomas.olvecky@gmail.com>
2026-02-19 07:36:48 +0100

    ci: Fix additional features variable syntax

--- scripts/test-phase2.sh
+++ scripts/test-phase2.sh
@@ -7,4 +7,4 @@ RUST_BACKTRACE=1 \
 RUST_LOG="${RUST_LOG:-info,obeli=debug,app=trace}" \
 cargo nextest run --no-output-indent --workspace --profile ci-test \
   ${ADDITIONAL_FEATURES:-} \
-  -- --skip populate_codegen_cache "$@"
+  -- --skip populate_codegen_cache --skip populate_js_codegen_cache "$@"
