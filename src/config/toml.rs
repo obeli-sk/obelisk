65e632a48b5949564c2a957cdf175dc6423c2c6e
Tomas Olvecky <tomas.olvecky@gmail.com>
2026-03-05 07:21:00 +0100

    refactor(toml)!: Remove `ignore_component_digest`

--- src/config/toml.rs
+++ src/config/toml.rs
@@ -1465,8 +1465,6 @@ pub(crate) struct ComponentBacktraceConfig {
     #[serde(rename = "sources")]
     #[schemars(with = "std::collections::HashMap<String, String>")]
     pub(crate) frame_files_to_sources: HashMap<String, String>,
-    #[serde(default)]
-    pub(crate) ignore_component_digest: bool,
 }
 impl ComponentBacktraceConfig {
     fn verify(self, path_prefixes: &PathPrefixes) -> ComponentBacktraceConfigVerified {
@@ -1490,7 +1488,6 @@ impl ComponentBacktraceConfig {
 
         ComponentBacktraceConfigVerified {
             frame_files_to_sources,
-            ignore_component_digest: self.ignore_component_digest,
         }
     }
 }
@@ -1498,7 +1495,6 @@ impl ComponentBacktraceConfig {
 #[derive(Debug)]
 pub(crate) struct ComponentBacktraceConfigVerified {
     pub(crate) frame_files_to_sources: HashMap<String, PathBuf>,
-    pub(crate) ignore_component_digest: bool,
 }
 
 #[derive(Debug)]
