a306938806c2c6a5f27dd0def5a5f5e5a5501fdd
Tomas Olvecky <tomas.olvecky@gmail.com>
2026-03-20 16:46:38 +0100

    refactor(workflow): Make `DeadlineTracker.track` respect actual deadline
    Previously `track` returned `None` when deadline - leeway was reached.
    This meant that a lot of time on `apply_inner` start the `deadline_minus_leeway`
    would not be reached, but then waiting for the blocking response could fall
    between `deadline_minus_leeway` and `deadline`. The previous impl would interrupt
    the run with "Giving up on waiting for response" message. New impl will keep
    tracking the response until actual `deadline` is reached, giving next `apply` call
    a chance to extend the lock.

--- crates/wasm-workers/src/workflow/deadline_tracker.rs
+++ crates/wasm-workers/src/workflow/deadline_tracker.rs
@@ -1,13 +1,16 @@
 use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 use concepts::{storage::TimeoutOutcome, time::ClockFn};
-use std::{cmp::min, pin::Pin, time::Duration};
+use std::{cmp::min, pin::Pin, sync::Arc, time::Duration};
 use tracing::{trace, warn};
 
 #[async_trait]
 pub trait DeadlineTracker: Send + Sync {
+
+    /// Called after the workflow made progress and is now blocked waiting for a response.
     /// Return a future that resolves on deadline. If `max_duration` is specified, the future resolves
     /// at deadline or after this duration, whatever expires first.
+    /// Return `None` if expired.
     fn track(
         &self,
         max_duration: Option<Duration>,
@@ -15,7 +18,7 @@ pub trait DeadlineTracker: Send + Sync {
 
     fn close_to_expired(&self) -> bool;
 
-    /// Return new lock expiry date (now + duration). Internally track that time minus leeway.
+    /// Called after `close_to_expired` returned `true`, Return new lock expiry date (now + duration). Internally track that time minus leeway.
     fn extend_by(&mut self, lock_extension: Duration) -> DateTime<Utc>;
 }
 
@@ -32,8 +35,10 @@ pub struct LockAlreadyExpired {
     pub started_at: DateTime<Utc>,
 }
 
+
 pub(crate) struct DeadlineTrackerTokio {
-    pub(crate) deadline: tokio::time::Instant, // Tracked as instant because calling track happens later after creation.
+    pub(crate) deadline: tokio::time::Instant,
+    pub(crate) deadline_minus_leeway: tokio::time::Instant, // Tracked as instant because calling track happens later after creation.
     pub(crate) clock_fn: Box<dyn ClockFn>,
     pub(crate) leeway: Duration, // Fire this much sooner than requested.
 }
@@ -44,38 +49,45 @@ impl DeadlineTracker for DeadlineTrackerTokio {
         &self,
         max_duration: Option<Duration>,
     ) -> Option<Pin<Box<dyn Future<Output = TimeoutOutcome> + Send>>> {
-        if self.close_to_expired() {
+        if self.deadline <= tokio::time::Instant::now() {
             None
         } else {
             let expiry = if let Some(max_duration) = max_duration {
                 let max_instant = tokio::time::Instant::now() + max_duration;
-                min(max_instant, self.deadline)
+                min(max_instant, self.deadline_minus_leeway)
             } else {
-                self.deadline
+                self.deadline_minus_leeway
             };
 
+            let mut interrupt_rx = self.hot_redeploy_signal.subscribe();
             Some(Box::pin(async move {
-                tokio::time::sleep_until(expiry).await;
-                TimeoutOutcome::Timeout
+                tokio::select! {
+                    () = tokio::time::sleep_until(expiry) => TimeoutOutcome::Timeout,
+                    _ = interrupt_rx.wait_for(|&v| v) => TimeoutOutcome::Timeout,
+                }
             }))
         }
     }
 
     fn close_to_expired(&self) -> bool {
-        self.deadline <= tokio::time::Instant::now()
+        self.deadline_minus_leeway <= tokio::time::Instant::now()
     }
 
     fn extend_by(&mut self, lock_extension: Duration) -> DateTime<Utc> {
-        let now = self.clock_fn.now();
+        let now_instant = tokio::time::Instant::now();
+        self.deadline = now_instant + lock_extension;
         let lock_duration = if lock_extension > self.leeway {
             lock_extension.checked_sub(self.leeway).unwrap()
         } else {
-            warn!("Not setting the leeway as deadline duration is too short");
+            warn!(
+                "Not setting the leeway as deadline duration {lock_extension:?} is shorter than leeway {:?}",
+                self.leeway
+            );
             lock_extension
         };
-        self.deadline = tokio::time::Instant::now() + lock_duration;
+        self.deadline_minus_leeway = now_instant + lock_duration;
 
-        now + lock_extension
+        self.clock_fn.now() + lock_extension
     }
 }
 
@@ -101,16 +113,20 @@ impl DeadlineTrackerFactory for DeadlineTrackerFactoryTokio {
         let Ok(deadline_duration) = (lock_expires_at - started_at).to_std() else {
             return Err(LockAlreadyExpired { started_at });
         };
-        let deadline_duration = if deadline_duration > self.leeway {
+        let now = tokio::time::Instant::now();
+        let deadline = now + deadline_duration;
+        let deadline_duration_minus_leeway = if deadline_duration > self.leeway {
             deadline_duration.checked_sub(self.leeway).unwrap()
         } else {
             warn!("Not setting the leeway as deadline duration is too short");
             deadline_duration
         };
-        trace!("Setting deadline to now + {deadline_duration:?}");
-        let deadline = tokio::time::Instant::now() + deadline_duration;
+        trace!("Setting deadline to now + {deadline_duration_minus_leeway:?}");
+
+        let deadline_minus_leeway = now + deadline_duration_minus_leeway;
         let tracker = DeadlineTrackerTokio {
             deadline,
+            deadline_minus_leeway,
             clock_fn: self.clock_fn.clone_box(),
             leeway: self.leeway,
         };
