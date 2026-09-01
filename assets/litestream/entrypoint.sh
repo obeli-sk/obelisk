e0f7111febb3f038d762a5093a1f0000323045c6
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-09-01 21:54:40 +0200

    doc: Move litestream config to examples, remove docker

--- assets/litestream/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-litestream restore -if-replica-exists --config litestream.yml ~/.local/share/obelisk/obelisk-sqlite/obelisk.sqlite
-exec litestream replicate --config litestream.yml --exec 'obelisk server run --litestream-socket /var/run/litestream.sock'
