124d72a31fec837ddf4ee332b84ed3e7086b10e8
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-09-06 14:52:11 +0200

    chore: Update assets hash

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
               nativeBuildInputs = with pkgs; [ skopeo jq cacert ];
               outputHashMode = "recursive";
               outputHashAlgo = "sha256";
-              outputHash = "sha256-W/Oea/zQjt9vAshGKE0/KqqFp7FYduLsbKy7ZdLxr74=";
+              outputHash = "sha256-b+iAhLfCaUdEtmmftJrkiH5D2GqXaq82QQcPEYPhkkw=";
               SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
               buildCommand = ''
                 mkdir -p "$out"
