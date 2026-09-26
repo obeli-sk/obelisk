0bc9aa9dcae9fc9f91b6a2ff77d5d3862e7776ad
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-09-26 09:25:03 +0200

    style: Clippy

--- crates/worker-common/src/js_imports.rs
+++ crates/worker-common/src/js_imports.rs
@@ -2,6 +2,7 @@ pub const SCHEDULE_SUFFIX: &str = "-obelisk-schedule";
 pub const EXT_SUFFIX: &str = "-obelisk-ext";
 pub const STUB_SUFFIX: &str = "-obelisk-stub";
 
+#[must_use]
 pub fn strip_specifier_suffix(specifier: &str, suffix: &str) -> Option<String> {
     let slash_pos = specifier.find('/')?;
     let pkg_part = &specifier[..slash_pos];
